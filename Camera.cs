e43dd9d116ace50e7ccb21e4b9190db77bc49876
Alanna Smith <smitalan240@gmail.com>
2024-05-15 14:41:30 -0400

    Add project files.

---
+++ Camera.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Final_Game___Space_Conquest
+{
+    public class Camera
+    {
+      
+
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,3 @@
+﻿
+using var game = new Final_Game___Space_Conquest.Game1();
+game.Run();
