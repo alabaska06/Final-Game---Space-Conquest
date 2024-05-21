78e9fb50987609af71ac017a0a37f5b17cfc8873
Alanna Smith <smitalan240@gmail.com>
2024-05-21 14:43:26 -0400

    added rotation but it didnt work

--- Player.cs
+++ Player.cs
@@ -14,17 +14,21 @@ namespace Final_Game___Space_Conquest
         public Vector2 Position;
         private Texture2D _texture;
         private float _speed;
+        private float _rotation;
 
         public Player(Texture2D texture)
         {
             _texture = texture;
             Position = Vector2.Zero;
             _speed = 2f;
+            _rotation = 0f;
         }
 
         public void Update(GameTime gameTime)
         {
             KeyboardState state = Keyboard.GetState();
+
+            Vector2 direction = Vector2.Zero;
 
             if (state.IsKeyDown(Keys.W))
                 Position.Y -= _speed;
@@ -34,11 +38,19 @@ namespace Final_Game___Space_Conquest
                 Position.X -= _speed;
             if (state.IsKeyDown(Keys.D))
                 Position.X += _speed;
+
+            if (direction != Vector2.Zero)
+            {
+                direction.Normalize();
+                Position += direction * _speed;
+                _rotation = (float)Math.Atan2(direction.Y, direction.X);
+            }
         }
 
         public void Draw(SpriteBatch spriteBatch)
         {
-            spriteBatch.Draw(_texture, Position, Color.White);
+            Vector2 origin = new Vector2(_texture.Width / 2, _texture.Height / 2);
+            spriteBatch.Draw(_texture, Position, null, Color.White, _rotation, origin, 1.0f, SpriteEffects.None, 0f);
         }
 
     }
